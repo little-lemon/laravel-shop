af80975673bf693da9bad6f9b6cbc3458789a86a
ying <766019882@qq.com>
2018-09-20 23:47:49 +0000

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('orders', 'OrdersController@store')->name('orders.store');
         Route::get('orders', 'OrdersController@index')->name('orders.index');
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+        Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+        Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     });
     // 结束
 });
@@ -42,6 +44,7 @@ Route::group(['middleware' => 'auth'], function() {
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 Route::get('alipay', function() {
     return app('alipay')->web([
